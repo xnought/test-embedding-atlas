833d6c68dd6e44d37f623046ac0520f6a19cf757
xnought <drbertucci@hotmail.com>
2025-05-22 00:18:48 -0400

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+	import * as vg from "@uwdata/vgplot";
+	import { EmbeddingAtlas } from "embedding-atlas/svelte";
+
+	const fetched = $derived.by(async () => {
+		vg.coordinator().databaseConnector(vg.wasmConnector());
+		await vg.coordinator().exec(vg.loadParquet("demo", "https://apple.github.io/embedding-atlas/demo/data/dataset.parquet"));
+
+		// print out the columns real fast (for debugging)
+		const out = await vg.coordinator().query("describe demo");
+		for (const col of out) {
+			console.log(col);
+		}
+	});
+</script>
+
+<main style:height="800px">
+	{#await fetched then}
+		<EmbeddingAtlas
+			coordinator={vg.coordinator()}
+			table="demo"
+			projectionColumns={{ x: "projection_x", y: "projection_y" }}
+			idColumn="_row_index"
+			textColumn="description"
+			automaticLabels={true}
+		/>
+	{/await}
+</main>
+
+<style>
+</style>
